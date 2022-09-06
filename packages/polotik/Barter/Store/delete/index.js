11f0899c401f6762ad0c08c287992ac7cb4fc64a
amirmohammad <86706357+AMSANJI-WORK@users.noreply.github.com>
2022-09-06 18:53:01 +0430

    Delete packages/polotik/Barter directory

--- packages/polotik/Barter/Store/delete/index.js
+++
@@ -1,7 +0,0 @@
-import deleteActions from "./actions";
-import deleteMutations from "./mutations";
-
-export {
-  deleteActions,
-  deleteMutations,
-};
--- packages/polotik/Barter/Store/modules/root/getters.js
+++
@@ -1,9 +0,0 @@
-const getters = {
-  barterList(state) {
-    return state.barterList;
-  },
-  barter(state) {
-    return state.barter;
-  },
-};
-export default getters;
