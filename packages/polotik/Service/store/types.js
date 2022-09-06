da8853a2ef6d5931385e767f0e79758674ad28f1
amirmohammad <86706357+AMSANJI-WORK@users.noreply.github.com>
2022-09-06 18:53:53 +0430

    Delete packages/polotik/Service directory

--- packages/polotik/Service/store/types.js
+++
@@ -1,9 +0,0 @@
-import * as categoryTypes from "./category/types";
-import * as citiesTypes from "./cities/types";
-import * as unitsTypes from "./unit/types";
-
-export default {
-  ...categoryTypes,
-  ...citiesTypes,
-  ...unitsTypes,
-};
