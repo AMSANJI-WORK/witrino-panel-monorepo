3e7f521ee29803e3b6dad5c82bfe114e7e0807aa
amirmohammad <86706357+AMSANJI-WORK@users.noreply.github.com>
2022-09-06 18:53:38 +0430

    Delete packages/polotik/Sale directory

--- packages/polotik/Sale/Middlewares/index.js
+++
@@ -1,6 +0,0 @@
-import store from "@polotik/store";
-export const resetSale = (to, from, next) => {
-  store.commit("guilds/sale/REST_SALE");
-  store.commit("upload/REST_IMAGES");
-  next();
-};
--- packages/polotik/Sale/modules/offers/mixins/request.js
+++
@@ -1,7 +0,0 @@
-import { mapGetters } from "vuex";
-const RequestMixin = {
-  computed: {
-    ...mapGetters("guilds/sale/request", ["user"]),
-  },
-};
-export default RequestMixin;
